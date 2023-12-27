fa4f103bf8f24c3467f3f7ea2169f2772911cca4
HeYaoDaDa <zhangwei.webber@gmail.com>
2023-12-27 21:06:05 +0800

    change modinfo name and description

--- src/ui/main_menu/show_new_game.rs
+++ src/ui/main_menu/show_new_game.rs
@@ -1,11 +1,14 @@
 use crate::ui::i18n::I18n;
 use crate::MainMenuState;
 use bevy::prelude::*;
+use bevy_fluent::Locale;
 use bevy_inspector_egui::bevy_egui::*;
 use serde::Deserialize;
 use std::{
+    collections::HashMap,
     fs::{self, File},
     io::Read,
+    ops::{Deref, DerefMut},
     path::PathBuf,
 };
 
@@ -13,28 +16,38 @@ pub fn show_new_game_system(
     mut ui: EguiContexts,
     i18n: Res<I18n>,
     mut next_menu_state: ResMut<NextState<MainMenuState>>,
+    mut modinfos: Local<Vec<Modinfo>>,
+    locale: Res<Locale>,
 ) {
     egui::Area::new("show_options")
         .movable(false)
         .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::default())
         .show(ui.ctx_mut(), |ui| {
             if ui.button(i18n.content("mods")).clicked() {
-                for modinfo in get_all_modinfo_json() {
-                    info!("{}:{}", modinfo.id, modinfo.path);
-                }
+                *modinfos.deref_mut() = get_all_modinfo_json();
             };
             if ui.button(i18n.content("start")).clicked() {};
             if ui.button(i18n.content("back")).clicked() {
                 next_menu_state.set(MainMenuState::MainMenu);
             }
         });
+    if !modinfos.is_empty() {
+        egui::Window::new(i18n.content("mods")).show(ui.ctx_mut(), |ui| {
+            for modinfo in modinfos.deref() {
+                ui.label(modinfo.get_name(&locale.requested.to_string()));
+            }
+            if ui.button(i18n.content("back")).clicked() {
+                modinfos.clear();
+            }
+        });
+    }
 }
 
 #[derive(Default, Deserialize)]
 pub struct Modinfo {
     pub id: String,
-    pub name: String,
-    pub description: String,
+    pub name: HashMap<String, String>,
+    pub description: HashMap<String, String>,
     pub authors: Vec<String>,
     pub maintainers: Vec<String>,
     pub version: String,
@@ -42,6 +55,30 @@ pub struct Modinfo {
     pub obsolete: bool,
     #[serde(skip)]
     pub path: String,
+}
+
+impl Modinfo {
+    fn get_name(&self, locale: &str) -> &String {
+        if self.name.is_empty() {
+            warn!("mod {} miss name", self.id);
+            &self.id
+        } else {
+            self.name
+                .get(locale)
+                .unwrap_or_else(|| self.name.get("en-US").unwrap())
+        }
+    }
+
+    fn get_description(&self, locale: &str) -> &String {
+        if self.description.is_empty() {
+            warn!("mod {} miss description", self.id);
+            &self.id
+        } else {
+            self.description
+                .get(locale)
+                .unwrap_or_else(|| self.description.get("en-US").unwrap())
+        }
+    }
 }
 
 pub fn get_all_modinfo_json() -> Vec<Modinfo> {
