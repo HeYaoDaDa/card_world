5479a671c755171a7fb99f990a1c412825d42f58
HeYaoDaDa <zhangwei.webber@gmail.com>
2024-01-20 16:59:11 +0800

    fianl tests generate_world

--- src/game/generate_world.rs
+++ src/game/generate_world.rs
@@ -2,34 +2,14 @@ use bevy::reflect::Reflect;
 use noise::{NoiseFn, Perlin};
 use rand::Rng;
 
-pub struct PerlinNoiseParams {
-    pub scale: f64,
-    pub max: i8,
-    pub min: i8,
-    pub weights: Vec<u8>,
-}
-
-fn generate_perlin_noise_elevation_ranges(min: i8, weights: &Vec<u8>) -> Vec<(f64, f64, i8)> {
-    let mut ranges = Vec::new();
-    let weights_sum: u64 = weights.iter().map(|&x| x as u64).sum();
-    let mut end: f64 = -1.;
-    for i in 0..weights.len() {
-        let start = end;
-        end = if i < weights.len() {
-            end + (*weights.get(i).unwrap() as f64 / weights_sum as f64) * 2.
-        } else {
-            2.
-        };
-        ranges.push((start, end, min + i as i8))
-    }
-    ranges
-}
-
 pub fn generate_elevation_map_with_perlin_noise(
     seed: Option<u32>,
     width: u8,
     height: u8,
-    params: PerlinNoiseParams,
+    scale: f64,
+    min: i8,
+    max: i8,
+    weights: &Vec<u8>,
 ) -> Result<Vec<Vec<i8>>, String> {
     if width == 0 {
         return Err(String::from("width cannot be zero"));
@@ -37,29 +17,29 @@ pub fn generate_elevation_map_with_perlin_noise(
     if height == 0 {
         return Err(String::from("height cannot be zero"));
     }
-    if params.min > params.max {
+    if min > max {
         return Err(String::from("min cannot be greater than max"));
     }
-    if (params.max - params.min + 1) as usize != params.weights.len() {
+    if (max - min + 1) as usize != weights.len() {
         return Err(String::from(
             "Number of weights does not match the difference between max and min",
         ));
     }
     let mut map = vec![vec![0; height as usize]; width as usize];
 
-    let perlin = Perlin::new(if let Some(seed) = seed {
+    let seed = if let Some(seed) = seed {
         seed
     } else {
         rand::thread_rng().gen::<u32>()
-    });
+    };
 
-    let ranges = generate_perlin_noise_elevation_ranges(params.min, &params.weights);
+    let noise = generate_perlin_noise(seed, width as usize, height as usize, scale);
+
+    let ranges = generate_perlin_noise_elevation_ranges(min, &weights);
     println!("ranges:{:#?}", ranges);
     for x in 0..width {
         for y in 0..height {
-            let nx = x as f64 / params.scale;
-            let ny = y as f64 / params.scale;
-            let value = perlin.get([nx, ny]);
+            let value = noise[x as usize][y as usize];
             for range in &ranges {
                 if range.0 <= value && value < range.1 {
                     map.get_mut(x as usize)
@@ -75,4 +55,36 @@ pub fn generate_elevation_map_with_perlin_noise(
     }
 
     Ok(map)
+}
+
+fn generate_perlin_noise(seed: u32, width: usize, height: usize, scale: f64) -> Vec<Vec<f64>> {
+    let perlin = Perlin::new(seed);
+
+    let mut noise_map = vec![vec![0.0; height]; width];
+
+    for x in 0..width {
+        for y in 0..height {
+            let nx = x as f64 / scale;
+            let ny = y as f64 / scale;
+            noise_map[x][y] = perlin.get([nx, ny]);
+        }
+    }
+
+    noise_map
+}
+
+fn generate_perlin_noise_elevation_ranges(min: i8, weights: &Vec<u8>) -> Vec<(f64, f64, i8)> {
+    let mut ranges = Vec::new();
+    let weights_sum: u64 = weights.iter().map(|&x| x as u64).sum();
+    let mut end: f64 = -1.;
+    for i in 0..weights.len() {
+        let start = end;
+        end = if i < weights.len() - 1 {
+            end + (*weights.get(i).unwrap() as f64 / weights_sum as f64) * 2.
+        } else {
+            f64::MAX
+        };
+        ranges.push((start, end, min + i as i8))
+    }
+    ranges
 }
--- src/ui/show_tests/show_tests_generate_world.rs
+++ src/ui/show_tests/show_tests_generate_world.rs
@@ -1,59 +1,151 @@
-use crate::{
-    game::generate_world::{self, PerlinNoiseParams},
-    ui::i18n::I18n,
-};
+use crate::{game::generate_world, ui::i18n::I18n};
 use bevy::prelude::*;
 use bevy_inspector_egui::{
     bevy_egui::*,
-    egui::{Color32, RichText},
+    egui::{vec2, Color32},
 };
 
 use super::TestsState;
+
+pub struct PerlinNoiseParamsUiState {
+    pub seed: Option<u32>,
+    pub width: u8,
+    pub height: u8,
+    pub scale: f64,
+    pub min: i8,
+    pub max: i8,
+    pub weights: Vec<u8>,
+    pub result: Option<Vec<Vec<i8>>>,
+}
+
+impl Default for PerlinNoiseParamsUiState {
+    fn default() -> Self {
+        Self {
+            seed: None,
+            width: 50,
+            height: 50,
+            scale: 10.,
+            min: -3,
+            max: 3,
+            weights: vec![10, 130, 70, 15, 40, 20, 10],
+            result: None,
+        }
+    }
+}
 
 pub fn show_tests_generate_world_system(
     mut ctx: EguiContexts,
     i18n: Res<I18n>,
     mut next_menu_state: ResMut<NextState<TestsState>>,
-    mut map: Local<Vec<Vec<i8>>>,
+    mut ui_state: Local<PerlinNoiseParamsUiState>,
 ) {
     egui::CentralPanel::default().show(ctx.ctx_mut(), |ui| {
         ui.vertical_centered_justified(|ui| {
             if ui.button(i18n.content("back")).clicked() {
                 next_menu_state.set(TestsState::Tests);
             }
-            if ui.button("start").clicked() {
-                let params = PerlinNoiseParams {
-                    scale: 10.,
-                    max: 3,
-                    min: -3,
-                    weights: vec![1, 2, 2, 2, 2, 2, 1],
+            ui.horizontal(|ui| {
+                ui.label("seed");
+                let mut seed_input = if let Some(seed) = ui_state.seed {
+                    seed.to_string()
+                } else {
+                    "".to_string()
                 };
-                *map =
-                    generate_world::generate_elevation_map_with_perlin_noise(None, 60, 50, params)
-                        .unwrap();
-            }
-            if !map.is_empty() {
+                ui.text_edit_singleline(&mut seed_input);
+                if let Ok(seed_input) = seed_input.parse() {
+                    ui_state.seed = Some(seed_input);
+                } else {
+                    ui_state.seed = None;
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.label("width");
+                let mut input = ui_state.width.to_string();
+                ui.text_edit_singleline(&mut input);
+                if let Ok(input) = input.parse() {
+                    ui_state.width = input;
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.label("height");
+                let mut input = ui_state.height.to_string();
+                ui.text_edit_singleline(&mut input);
+                if let Ok(input) = input.parse() {
+                    ui_state.height = input;
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.label("scale");
+                let mut input = ui_state.scale.to_string();
+                ui.text_edit_singleline(&mut input);
+                if let Ok(input) = input.parse() {
+                    ui_state.scale = input;
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.label("min");
+                let mut input = ui_state.min.to_string();
+                ui.text_edit_singleline(&mut input);
+                if let Ok(input) = input.parse() {
+                    ui_state.min = input;
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.label("max");
+                let mut input = ui_state.max.to_string();
+                ui.text_edit_singleline(&mut input);
+                if let Ok(input) = input.parse() {
+                    ui_state.max = input;
+                }
+            });
+            for i in 0..=ui_state.max - ui_state.min {
                 ui.horizontal(|ui| {
-                    for column in &map {
-                        ui.vertical(|ui| {
-                            for item in column.iter().rev() {
-                                if *item >= 0 {
-                                    ui.label(
-                                        RichText::new(item.to_string()).color(Color32::from_rgb(
-                                            0,
-                                            item.abs() as u8 * 25,
-                                            0,
-                                        )),
-                                    );
-                                } else {
-                                    ui.label(
-                                        RichText::new(item.abs().to_string())
-                                            .color(Color32::from_rgb(0, 0, item.abs() as u8 * 25)),
-                                    );
-                                }
-                            }
-                        });
+                    let height = ui_state.min + i;
+                    let weight = ui_state.weights[i as usize];
+                    ui.label(height.to_string());
+                    let mut input = weight.to_string();
+                    ui.text_edit_singleline(&mut input);
+                    if let Ok(input) = input.parse() {
+                        ui_state.weights[i as usize] = input;
                     }
+                });
+            }
+            if ui.button("start").clicked() {
+                ui_state.result = Some(
+                    generate_world::generate_elevation_map_with_perlin_noise(
+                        ui_state.seed,
+                        ui_state.width,
+                        ui_state.height,
+                        ui_state.scale,
+                        ui_state.min,
+                        ui_state.max,
+                        &ui_state.weights,
+                    )
+                    .unwrap(),
+                );
+            }
+            if ui_state.result.is_some() {
+                egui::ScrollArea::both().show(ui, |ui| {
+                    ui.horizontal(|ui| {
+                        ui.spacing_mut().item_spacing = vec2(0.0, 0.0);
+                        ui.style_mut().visuals.override_text_color = Some(Color32::WHITE);
+                        for column in ui_state.result.clone().unwrap() {
+                            ui.vertical(|ui| {
+                                for item in column.iter().rev() {
+                                    let background_color = if *item > 0 {
+                                        Color32::from_rgb(0, item.abs() as u8 * 80, 0)
+                                    } else {
+                                        Color32::from_rgb(0, 0, item.abs() as u8 * 80)
+                                    };
+                                    egui::Frame::none().fill(background_color).show(ui, |ui| {
+                                        ui.set_width(20.);
+                                        ui.set_height(20.);
+                                        ui.label(item.to_string());
+                                    });
+                                }
+                            });
+                        }
+                    });
                 });
             }
         });
